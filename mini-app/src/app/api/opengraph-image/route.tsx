22446455bc40e79ba4281136741697de1fa3e757
Godwin <godwinaquinas@gmail.com>
2025-06-27 00:39:55 +0100

    update frame action button text

--- mini-app/src/app/api/opengraph-image/route.tsx
+++ mini-app/src/app/api/opengraph-image/route.tsx
@@ -13,8 +13,7 @@ export async function GET() {
           <div tw="flex w-96 h-96 rounded-full overflow-hidden mb-8 border-8 border-white">
             <img src={imgSrc} alt="Profile" tw="w-full h-full object-cover" />
           </div>
-        <h1 tw="text-8xl text-white">Mint your fav a coffee!</h1>
-        <p tw="text-5xl mt-4 text-white opacity-80">Powered by Mintmeacoffe.com</p>
+        <h1 tw="text-8xl text-white">Tip with a Coffee, Mint Collectibles</h1>
       </div>
     ),
     {
